c633becbd4e759d574decfab159a4bf766e96299
callmekund <gauravbanna5555@gmail.com>
2025-05-02 15:19:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -62,8 +62,8 @@ OWNER_UPI_ID = environ.get('OWNER_UPI_ID', '')
 # MongoDB Configuration
 # ============================
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://gauravbanna5555:gauravbanna5555@cluster0.7dsjxra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "gauravbanna5555")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://gaurav:gaurav@gaurav.7pjoig5.mongodb.net/?retryWrites=true&w=majority&appName=gaurav")
+DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0 gaurav")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Gaurav_files')
 
 # ============================
